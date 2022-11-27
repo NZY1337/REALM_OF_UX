fa0a4a5050101fe4aaea7672dc7a71e12253c843
NZY1337 <mandreicosmin@yahoo.com>
2022-11-27 22:56:19 +0200

    code-editor -> wip (#8)

---
+++ client/src/pages/CodeEditor/Editor.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import {Controlled as CodeMirror} from 'react-codemirror2'
+import { FaCompressAlt, FaExpandAlt} from 'react-icons/fa'
+
+export default function Editor(props) {
+  const {
+    language,
+    displayName,
+    value,
+    onChange
+  } = props
+  const [open, setOpen] = useState(true)
+
+  function handleChange(editor, data, value) {
+    onChange(value)
+  }
+
+  return (
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen(prevOpen => !prevOpen)}
+        >
+        {open ? < FaCompressAlt/> : <FaExpandAlt />}
+        </button>
+      </div>
+      <CodeMirror
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true
+        }}
+      />
+    </div>
+  )
+}
