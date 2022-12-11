b7d7494f2aeaf122c3ddd70a21f18a04d6c61172
andrei.mocanu <andrei.mocanu@boots.co.uk>
2022-12-11 16:56:21 +0200

    refactored translate context

--- client/src/utils/contexts/translate/lang/EN.js
+++ client/src/utils/contexts/translate/lang/EN.js
@@ -1,14 +1,16 @@
 //dictionaries
-import { aboutMeDict } from "../../../../pages/About/translate";
-import { footerDict } from "../../../../components/Footer/translate";
-import { sliderDict } from "../../../../pages/Home/Slider/translate";
-import { aboutHomeDict } from "../../../../pages/Home/About/translate";
-import { servicesHomeDict } from "../../../../pages/Home/Services/translate";
-import { bannerHomeDict } from "../../../../pages/Home/Banner/translate";
-import { contactHomeDict } from "../../../../pages/Home/ContactUs/translate";
-import { navigationDict } from "../../../../components/Navigation/translate";
-import { loginRegisterDict } from "../../../../pages/LoginRegister/translate";
-import { homeProjectsDict } from "../../../../pages/Home/Projects/translate";
+import {
+  aboutMeDict,
+  footerDict,
+  sliderDict,
+  aboutHomeDict,
+  servicesHomeDict,
+  bannerHomeDict,
+  contactHomeDict,
+  navigationDict,
+  loginRegisterDict,
+  homeProjectsDict,
+} from "./index.js";
 
 const initialStateEN = {
   globals: {
---
+++ client/src/utils/contexts/translate/lang/index.js
@@ -0,0 +1,23 @@
+import { aboutMeDict } from "../../../../pages/About/translate";
+import { footerDict } from "../../../../components/Footer/translate";
+import { sliderDict } from "../../../../pages/Home/Slider/translate";
+import { aboutHomeDict } from "../../../../pages/Home/About/translate";
+import { servicesHomeDict } from "../../../../pages/Home/Services/translate";
+import { bannerHomeDict } from "../../../../pages/Home/Banner/translate";
+import { contactHomeDict } from "../../../../pages/Home/ContactUs/translate";
+import { navigationDict } from "../../../../components/Navigation/translate";
+import { loginRegisterDict } from "../../../../pages/LoginRegister/translate";
+import { homeProjectsDict } from "../../../../pages/Home/Projects/translate";
+
+export {
+  aboutMeDict,
+  footerDict,
+  sliderDict,
+  aboutHomeDict,
+  servicesHomeDict,
+  bannerHomeDict,
+  contactHomeDict,
+  navigationDict,
+  loginRegisterDict,
+  homeProjectsDict,
+};
