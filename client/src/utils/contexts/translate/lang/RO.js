eab8105855157f79b10f381b4715e7b3d359ac7a
NZY1337 <mandreicosmin@yahoo.com>
2022-12-11 16:59:13 +0200

    Feature/translate context (#17)
    * Translating pages .. WIP
    
    * translation added for HomePage... WIP
    
    * refactored translate context
    
    Co-authored-by: andrei.mocanu <andrei.mocanu@boots.co.uk>

--- client/src/utils/contexts/translate/lang/RO.js
+++ client/src/utils/contexts/translate/lang/RO.js
@@ -1,30 +1,16 @@
-// about
-import { Eye, Envelope, Person, Phone, Mailbox } from "react-bootstrap-icons";
-// services
-import seo from "../../../../assets/images/seo.svg";
-import media from "../../../../assets/images/ads.svg";
-import development from "../../../../assets/images/dev.svg";
-import social from "../../../../assets/images/social.svg";
-
-// blog
-import img2 from "../../../../assets/images/img2.jpeg";
-import img4 from "../../../../assets/images/img4.jpeg";
-import img5 from "../../../../assets/images/img5.jpeg";
-import img6 from "../../../../assets/images/img6.jpeg";
-import img9 from "../../../../assets/images/img20.jpeg";
-import img10 from "../../../../assets/images/img10.jpeg";
-
 // dictionaries
-import { aboutMeDict } from "../../../../pages/About/translate";
-import { footerDict } from "../../../../components/Footer/translate";
-import { sliderDict } from "../../../../pages/Home/Slider/translate";
-import { aboutHomeDict } from "../../../../pages/Home/About/translate";
-import { servicesHomeDict } from "../../../../pages/Home/Services/translate";
-import { bannerHomeDict } from "../../../../pages/Home/Banner/translate";
-import { contactHomeDict } from "../../../../pages/Home/ContactUs/translate";
-import { navigationDict } from "../../../../components/Navigation/translate";
-import { loginRegisterDict } from "../../../../pages/LoginRegister/translate";
-import { homeProjectsDict } from "../../../../pages/Home/Projects/translate";
+import {
+  aboutMeDict,
+  footerDict,
+  sliderDict,
+  aboutHomeDict,
+  servicesHomeDict,
+  bannerHomeDict,
+  contactHomeDict,
+  navigationDict,
+  loginRegisterDict,
+  homeProjectsDict,
+} from "./index.js";
 
 // Do I really have to clone this?
 const initialStateRO = {
